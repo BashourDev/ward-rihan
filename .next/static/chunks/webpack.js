724754bef82c1fb51c7a74ca01373c1e4bc78425
BashourDev <102165792+BashourDev@users.noreply.github.com>
2025-08-18 15:29:36 +0400

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3946b0f292e1dccd"; }
+/******/ 		__webpack_require__.h = function() { return "37772fb56c433c97"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
